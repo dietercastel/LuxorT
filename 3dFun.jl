3b82cb7323cf8636353739d1f80f8f0352081469
Dieter Castel <dietercastel@gmail.com>
2020-04-25 21:35:47 +0200

    Bugfix and added gif.

--- 3dFun.jl
+++ 3dFun.jl
@@ -1,14 +1,14 @@
 using Plots
 
+theme(:dark)
 function between(p1,p2,offset)
 	return p1+(offset*(p2-p1))
 end
 
-
 p1 = [1.0 ; 0.0 ; -1/sqrt(2)]
 p2 = [-1.0 ; 0.0 ; -1/sqrt(2)]
-p3 = [0.0 ; 1.0 ; -1/sqrt(2)]
-p4 = [0.0 ; -1.0 ; -1/sqrt(2)]
+p3 = [0.0 ; 1.0 ; 1/sqrt(2)]
+p4 = [0.0 ; -1.0 ; 1/sqrt(2)]
 
 its = 10000
 guidePoints = [p1,p2,p3,p4]
@@ -41,8 +41,9 @@ println(size(points))
 #println(points[2,:])
 #println(points[3,:])
 anim = @animate for i in range(0, stop = 2π, length= 100)
-	p = Plots.plot(points[1,:],points[2,:],points[3,:], seriestype = :scatter, group = group)
-	Plots.plot!(p, camera = (10 * (1 + cos(i)), 40))
+	p = Plots.plot(points[1,:],points[2,:],points[3,:], seriestype = :scatter, group = group, axis=nothing)
+	#Plots.plot!(p, camera = (10 * (1 + cos(i)), 40))
+	Plots.plot!(p, camera = (10 * (1 + cos(i)), 10*(1+cos(i))))
 end
 
 gif(anim, "3dfun.gif", fps=10)

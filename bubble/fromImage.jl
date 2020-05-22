8bfaec76a89e7ada4e8e70f8e39483d1773143ed
Dieter Castel <dietercastel@gmail.com>
2020-05-22 10:06:03 +0200

    Added bubble image experiment.

--- bubble/fromImage.jl
+++ bubble/fromImage.jl
@@ -1,30 +1,37 @@
 using Images, FileIO, Colors, Plots
 using Luxor
 img = load("Crop1.png")
-imedge(img)
 img_gray = Gray.(img)
-imedge(img_gray)
-save("crop1_grey.png")
 save("crop1_grey.png", img_gray)
 img2 = load("Crop2.png")
 img_gray2 = Gray.(img2)
 save("crop2_grey.png", img_gray2)
-imedge(img_gray)
-imedge(img_gray)[1]
 size(imedge(img_gray)[1])
 gx, gy, mag, or = imedge(img_gray)
 maximum(or)
 y,x = size(mag)
 yarr =vcat([repeat([i],928) for i in 1:1774]...)
-plot(repeat(1:928,1774),yarr,reshape(mag,(928*1774,)),st=:surface)
-save("sobel1.png",mag)
+#plot(repeat(1:928,1774),yarr,reshape(mag,(928*1774,)),st=:surface)
+#save("sobel1.png",mag)
 
-function luxorIdea()
-	origin()
-	for y in 1:1774 
-		for x in 1:928
-			rule(Point(x, y), or[y,x])
+function luxorIdea(mag,or)
+	maxY,maxX = size(or)
+	maxMag = maximum(mag)
+	Drawing()
+	#origin()
+	r1 = rect(Point(0,0),maxX,maxY,:stroke)
+	bb = BoundingBox(r1)
+
+	δ = 0.0001
+	for y in 1:maxY
+		for x in 1:maxX
+			perct = mag[y,x]/maxMag
+			sethue((perct*255,perct*100,perct*55))
+			rule(Point(x, y), or[y,x],boundingbox=bb)
 			# vector field with Luxor using arrow
 		end
 	end
 end
+luxorIdea(mag,or)
+finish()
+preview()

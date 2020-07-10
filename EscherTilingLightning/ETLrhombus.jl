c83caa2d6222aa820251af21740daca0fafd3b33
Dieter Castel <dietercastel@gmail.com>
2020-07-10 13:34:31 +0200

    Tweaking the rhomubs ETL.

--- EscherTilingLightning/ETLrhombus.jl
+++ EscherTilingLightning/ETLrhombus.jl
@@ -16,7 +16,7 @@ do an action.
 
 Use `vertices=true` to return an array of the four corner points: left, top, right, bottom.
 """
-function rhombus(centerpoint::Point, width, height, action=:nothing; vertices=false)
+function rhombus(centerpoint::Point, width, height, action=:none; vertices=false)
 	 ptlist = [centerpoint - (width/2,0),
 						 centerpoint - (0,height/2),
 						 centerpoint + (width/2,0),
@@ -27,7 +27,7 @@ function rhombus(centerpoint::Point, width, height, action=:nothing; vertices=fa
 	 return ptlist
 end
 
-#TODO
+# upright grid drawing
 function mandraw(grid)
 	Drawing()
 	w=35
@@ -48,6 +48,85 @@ function mandraw(grid)
 	preview()
 end
 
+
+#45 grid drawing
+function my45edGrid(grid)
+		Luxor.rotate(-π/4)
+		w=35
+		h=20
+		for x in 1:grid[1]
+			 for y in 1:grid[2]
+					 #drawRhombus(x,x+(y-1),w,h,:stroke)
+					 drawRhombusCompl(x,x+(y-1),w,h,:stroke)
+			 end
+	 end
+end
+
+function drawRhombus(x,y,w,h,action=:none;vertices=false, color=:none)
+	offset=w/2+h/2
+	if isodd(x) & isodd(y)
+		color == :none ? setcolor("darkgreen") : setcolor(color)
+		return rhombus(Point(offset*x,offset*y),w,h,action,vertices=vertices)
+	end
+	if iseven(x) & isodd(y)
+		color == :none ? setcolor("darkblue") : setcolor(color)
+		return rhombus(Point(offset*x,offset*y),h,w,action,vertices=vertices)
+	end
+	if isodd(x) & iseven(y)
+		color == :none ? setcolor("darkblue") : setcolor(color)
+		return rhombus(Point(offset*x,offset*y),h,w,action,vertices=vertices)
+	end
+	if iseven(x) & iseven(y) 
+		color == :none ? setcolor("darkgreen") : setcolor(color)
+		return rhombus(Point(offset*x,offset*y),w,h,action,vertices=vertices)
+	end
+end
+
+function drawRhombusCompl(x,y,w,h,action=:none;vertices=false, color=:none)
+	offset=w/2+h/2
+	if isodd(x) & isodd(y) # (1,1)
+		color == :none ? setcolor("darkgreen") : setcolor(color)
+		return rhombus(Point(offset*x,offset*y),w,h,action,vertices=vertices)
+	end
+	if iseven(x) & isodd(y) # (2,1)
+		#Draw complementing polygon between two rhombhi on the same row.
+		color == :none ? setcolor("darkblue") : setcolor(color)
+		prevRhomb =rhombus(Point(offset*(x-1),offset*y),w,h,vertices=true)
+		nextRhomb =rhombus(Point(offset*(x+1),offset*y),w,h,vertices=true)
+		complVertices = [prevRhomb[4],prevRhomb[3],nextRhomb[2],nextRhomb[1]]
+		if vertices == true
+			return complVertices
+		else
+			return poly(complVertices, action, close=true)
+		end
+	end
+	if isodd(x) & iseven(y) # (1,2)
+		#Draw complementing polygon between two rhombhi on the same row.
+		color == :none ? setcolor("darkblue") : setcolor(color)
+		prevRhomb =rhombus(Point(offset*(x-1),offset*y),h,w,vertices=true)
+		println("prev $prevRhomb")
+		nextRhomb =rhombus(Point(offset*(x+1),offset*y),h,w,vertices=true)
+		println("next $nextRhomb")
+		complVertices = [prevRhomb[4],prevRhomb[3],nextRhomb[2],nextRhomb[1]]
+		println("taking $complVertices")
+		if vertices == true
+			return complVertices
+		else
+			return poly(complVertices, action, close=true)
+		end
+	end
+	if iseven(x) & iseven(y) # (2,2) 
+		color == :none ? setcolor("darkgreen") : setcolor(color)
+		return rhombus(Point(offset*x,offset*y),h,w,action,vertices=vertices)
+	end
+end
+
+function test(grid)
+	Drawing()
+	my45edGrid(grid)
+	finish()
+	preview()
+end
 
 function remEdgeProb!(g,e,edgeRemProb)
 	if rand()<=edgeRemProb
@@ -87,18 +166,20 @@ function drawRelNb(relNB::Tuple, box::Array)
 	line(box[idxs[1]],box[idxs[2]],:stroke)	
 end
 
-function draw(size,tileSize;edgeRemProb=0.8)
+function draw(size,tileSize;edgeRemProb=0.8, rotation=-π/4)
 	gridSize = size 
 	g2, xs, ys = generateTiles(gridSize,edgeRemProb)	
 	tileW,tileH = tileSize
+	Luxor.rotate(rotation)
 	#= origin() =#
 	for v in vertices(g2)
 		nbs = neighbors(g2,v)
 		#= println(nbs) =#
 		relNb = neighborsToRelCoord(v,nbs,gridSize)
-		x = xs[v]*tileW
-		y = ys[v]*tileH
-		rectBox = box(Point(x,y),tileW,tileH,vertices=true)
+		x = xs[v]
+		y = ys[v]
+		rectBox = drawRhombus(x,x+(y-1),tileW,tileH,vertices=true)
+		#= rectBox = drawRhombusCompl(x,x+(y-1),tileW,tileH,vertices=true) =#
 		# Draw a border where there is NO neighbor connection
 		borders = filter(x-> x ∉ relNb, allRelNbs)
 		map(nb->drawRelNb(nb,rectBox),borders)
@@ -128,17 +209,24 @@ function drawLightning(g,xs,ys,size, tileSize)
 	filter!(x->length(x) != 0, sortedPaths)
 	println("Lightning= ", sortedPaths[1])
 	lightningPath = sortedPaths[1]
-	sethue("white")
+#	sethue("white")
 	for v in lightningPath
-		x = xs[v]*tileW
-		y = ys[v]*tileH
-		rectBox = box(Point(x,y),tileW,tileH,:fill)
+		x = xs[v]
+		y = ys[v]
+		#= rectBox = drawRhombusCompl(x,x+(y-1),tileW,tileH,:fill,color="white") =#
+		#
+		rectBox = drawRhombus(x,x+(y-1),tileW,tileH,:fill,color="white")
+		#
+		rectBox = drawRhombus(x+1,x+(y-1),tileW,tileH,:fill,color="white")
+		rectBox = drawRhombus(x-1,x+(y-1),tileW,tileH,:fill,color="white")
+		rectBox = drawRhombus(x,x+(y),tileW,tileH,:fill,color="white")
+		rectBox = drawRhombus(x,x+(y-2),tileW,tileH,:fill,color="white")
 	end
 end
 
 function main()
-	Drawing()
-	draw((40,40),(10,10),edgeRemProb=0.4)
+	Drawing(1600,800)
+	draw((30,30),(35,20),edgeRemProb=0.3,rotation=-π/6)
 	finish()
 	preview()
 end
